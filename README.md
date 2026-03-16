1f3c2231d2df558eb53418d6c7fb5ce643a9270b
Jacob Liu <flcker@users.noreply.github.com>
2026-03-16 09:42:27 +0000

    README
    pwsh 7 config file
    
    增加init_profile_dependencies.ps1,用来安装Module与tools;
    
    + add sub shell

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Shell_Config
 终端配置文件
+
+## branch
+
+|分支|说明|
+| :---: | :--- |
+| main | 空置分支, 用来写说明 |
+|pwsh | PowerShell 7的配置文件|
+|zsh | zsh 的配置 | 
+
+## pwsh 安装、依赖与配置
+
+[./pwsh.md]
+
+
+## zsh 安装、依赖与配置
+
+[./zsh.md]
+
---
+++ sh/aliases_and_functions.ps1
@@ -0,0 +1,28 @@
+################################################################################
+# functions and aliases
+function ls_fun { lsd.exe @args }
+function ll_fun { lsd.exe -alF @args }
+function la_fun { lsd.exe -a @args }
+function lr_fun { lsd.exe -R @args }
+
+Set-Alias ls ls_fun -Option AllScope
+Set-Alias ll ll_fun -Option AllScope
+Set-Alias la la_fun -Option AllScope
+Set-Alias lr lr_fun -Option AllScope
+Set-Alias ~ Set-Location -Option AllScope
+Set-Alias vim nvim -Option AllScope
+Set-Alias cat bat.exe
+Set-Alias clcb clcb_fun -Option AllScope
+Set-Alias grep Select-String -Option AllScope
+Set-Alias rm Remove-Item -Option AllScope
+Set-Alias mv Move-Item -Option AllScope
+Set-Alias cp Copy-Item -Option AllScope
+Set-Alias mkdir New-Item -Option AllScope
+Set-Alias rmdir Remove-Item -Option AllScope
+Set-Alias touch New-Item -Option AllScope
+Set-Alias find Get-ChildItem -Option AllScope
+Set-Alias htop pstop
+Set-Alias zo zoxide
+Set-Alias lg lazygit
+
+function clcb_fun { Set-Clipboard "" }
