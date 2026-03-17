c1e974890437badd429191ad1fb4099a345a29a5
flcker <molanyousi@gmail.com>
2026-03-17 11:23:08 +0800

    *

--- sh/aliases_and_functions.ps1
+++ sh/aliases_and_functions.ps1
@@ -5,6 +5,9 @@ function ll_fun { lsd.exe -alF @args }
 function la_fun { lsd.exe -a @args }
 function lr_fun { lsd.exe -R @args }
 
+# clear clipboard
+function clcb_fun { Set-Clipboard "" }
+
 Set-Alias ls ls_fun -Option AllScope
 Set-Alias ll ll_fun -Option AllScope
 Set-Alias la la_fun -Option AllScope
@@ -25,4 +28,3 @@ Set-Alias htop pstop
 Set-Alias zo zoxide
 Set-Alias lg lazygit
 
-function clcb_fun { Set-Clipboard "" }
