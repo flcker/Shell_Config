ede4d208d0c04216be373a43c3fadb123fedd4b3
liujiankang2 <liujiankang2@tal.com>
2026-04-02 17:40:12 +0800

    zshrc rename zshrc.sh; zsh：修改风格；增加nvim； submodule：nvim；

---
+++ zshrc.zsh
@@ -0,0 +1,25 @@
+################################################################################
+# 需要在 "~/.zshrc" 中调用
+################################################################################
+
+# config dir vars
+ZSH_CONFIG_DIR="${ZSH:-$HOME/.config/zsh}"
+ZSH_CONFIG_STARSHIP_DIR="$ZSH_CONFIG_DIR/submodule/starship"
+ZSH_CONFIG_NVIM_DIR="$ZSH_CONFIG_DIR/submodule/nvim"
+
+# module loader
+ZSH_MODULE_DIR="$HOME/.config/zsh/zsh"
+
+for config_file in "$ZSH_MODULE_DIR"/*.zsh; do
+  if [[ -f "$config_file" ]]; then
+    source "$config_file"
+  fi
+done
+
+
+# plugins
+plugins=(
+  git
+  zsh-autosuggestions
+  zsh-syntax-highlighting
+)
